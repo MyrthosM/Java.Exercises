038eb6236a095dd868a9e09d6d6f0523a4827e3d
Miroslav Ivanov <mirochoni@gmail.com>
2016-11-30 14:37:56 +0000

    Update

--- src/Exercices/Chapter-2/Exercise225.java
+++ src/Exercices/Chapter-2/Exercise225.java
@@ -1,11 +1,14 @@
-// Prentice Hall Java 10 book excercise 2.**
-/* Write an application that asks the user to enter two integers, obtains them from the user and displays the larger number.
-Followed by the words "is larger". If the numbers are equal, print the message "These numbers are equal".*/
+// Prentice Hall Java 10 book excercise 2.25
+/* Write an application that reads an integer and determines and prints whether it’s odd or even.
+[Hint: Use the remainder operator. An even number is a multiple of 2. Any multiple of 2 leaves a remainder of 0 when divided by 2.*/
 
 import java.util.Scanner; // Importing the library for obtaining user input
 
-public class Exercise2** {
+public class Exercise225 {
 	public static void main (String [] args) {
 		//Declaration and initzialization of the program variables.
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a whole number : ");
 	}
 }
