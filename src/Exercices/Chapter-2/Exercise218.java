6226481b98387f08df108c0ddc10b83d6fed8f09
Miroslav Ivanov <mirochoni@gmail.com>
2016-11-30 14:20:07 +0000

    Update 30/11

---
+++ src/Exercices/Chapter-2/Exercise218.java
@@ -0,0 +1,28 @@
+// Prentice Hall Java 10 book excercise 2.18
+/* Write an application that displays a box, an oval, an arrow and a diamond using asterisks (*)*/
+
+
+
+public class Exercise218 {
+	public static void main (String [] args) {
+		
+		System.out.println("*********	***");
+		System.out.println("*	*	*	*");
+		System.out.println("*	*");
+		System.out.println("*	*");
+		System.out.println("*	*");
+		System.out.println("*	*");
+		System.out.println("*	*");
+		System.out.println("*********");
+	
+		System.out.println("	***	");
+		System.out.println("*********");
+		System.out.println("*********");
+		System.out.println("*********");
+		System.out.println("*********");
+		System.out.println("*********");
+		System.out.println("	*	*	");
+		System.out.println("	***	");
+		
+	}
+}
